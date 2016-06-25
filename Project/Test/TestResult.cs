41b99f9f8427f51ccad2aaf12e1b417a01e56dea
石川達也 <stone.guitar.777@gmail.com>
2016-06-26 08:58:54 +0900

    add tests.

---
+++ Project/Test/TestResult.cs
@@ -0,0 +1,24 @@
+﻿using LambdicSql;
+using LambdicSql.QueryInfo;
+using System;
+using System.Collections.Generic;
+
+namespace Test
+{
+    class TestResult : IDbResult
+    {
+        public Dictionary<string, object> Data { get; } = new Dictionary<string, object>();
+        public object this[string key] { get { return Data[key]; } set { Data[key] = value; } }
+
+        public DateTime GetDateTime(string name) => (DateTime)Data[name];
+        public int GetInt32(string name) => (int)Data[name];
+        public string GetString(string name) => (string)Data[name];
+
+        internal T Create<T>(IQuery<T, T> query)
+            where T : class
+        {
+            var info = query as IQueryInfo<T>;
+            return info.Create(this);
+        }
+    }
+}
--- Project/Test/TestUsingLambda.cs
+++ Project/Test/TestUsingLambda.cs
@@ -2,7 +2,7 @@
 using LambdicSql.QueryInfo;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
-using System.Linq;
+using System.Collections.Generic;
 
 namespace Test
 {
@@ -75,9 +75,108 @@ namespace Test
             table2.Children.Count.Is(2);
             table2.Children["col4"].Type.Is(typeof(int));
         }
+        
+        [TestMethod]
+        public void Schema()
+        {
+            var query = Sql.Using(() => new
+            {
+                table1 = new
+                {
+                    col1 = "",
+                    col2 = 0
+                },
+                dbo = new
+                {
+                    table2 = new
+                    {
+                        col3 = "",
+                        col4 = 0
+                    }
+                }
+            });
+            var info = query as IQueryInfo;
 
-        //schema
+            info.Db.Children.Count.Is(2);
 
-        //create
+            var empty = info.Db.Children[""];
+            empty.Children.Count.Is(1);
+
+            var table1 = empty.Children["table1"];
+            table1.Children.Count.Is(2);
+            var col1 = table1.Children["col1"];
+            col1.FullName.Is(new[] { "table1", "col1" });
+            col1.FullNameText.Is("table1.col1");
+            col1.Type.Is(typeof(string));
+            var col2 = table1.Children["col2"];
+            col2.FullNameText.Is("table1.col2");
+
+            var dbo = info.Db.Children["dbo"];
+            dbo.Children.Count.Is(1);
+            var table2 = dbo.Children["table2"];
+            table2.Children.Count.Is(2);
+            table2.Children["col4"].Type.Is(typeof(int));
+        }
+
+        [TestMethod]
+        public void CreateTableOnly()
+        {
+            var query = Sql.Using(() => new
+            {
+                table1 = new
+                {
+                    col1 = "",
+                    col2 = 0
+                },
+                table2 = new
+                {
+                    col3 = "",
+                    col4 = 0
+                }
+            });
+
+            var data = new TestResult();
+            data["table1.col1"] = "abc";
+            data["table1.col2"] = 100;
+            data["table2.col3"] = "def";
+            data["table2.col4"] = 200;
+            var obj = data.Create(query);
+            obj.table1.col1.Is("abc");
+            obj.table1.col2.Is(100);
+            obj.table2.col3.Is("def");
+            obj.table2.col4.Is(200);
+        }
+
+        [TestMethod]
+        public void CreateSchema()
+        {
+            var query = Sql.Using(() => new
+            {
+                table1 = new
+                {
+                    col1 = "",
+                    col2 = 0
+                },
+                dbo = new
+                {
+                    table2 = new
+                    {
+                        col3 = "",
+                        col4 = 0
+                    }
+                }
+            });
+
+            var data = new TestResult();
+            data["table1.col1"] = "abc";
+            data["table1.col2"] = 100;
+            data["dbo.table2.col3"] = "def";
+            data["dbo.table2.col4"] = 200;
+            var obj = data.Create(query);
+            obj.table1.col1.Is("abc");
+            obj.table1.col2.Is(100);
+            obj.dbo.table2.col3.Is("def");
+            obj.dbo.table2.col4.Is(200);
+        }
     }
 }
