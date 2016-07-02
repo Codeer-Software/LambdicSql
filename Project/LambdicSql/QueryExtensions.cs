4d4a4f8921415fea126011fac942e2eb5e599db2
石川達也 <stone.guitar.777@gmail.com>
2016-07-02 09:55:45 +0900

    add expression message.

--- Project/LambdicSql/QueryExtensions.cs
+++ Project/LambdicSql/QueryExtensions.cs
@@ -1,16 +1,22 @@
 ﻿using LambdicSql.Inside;
 using LambdicSql.QueryBase;
+using System;
 
 namespace LambdicSql
 {
     public static class QueryExtensions
     {
-        public static T Cast<T>(this IQuery query) => default(T);
+        public static T Cast<T>(this IQuery query)
+        {
+            throw new NotSupportedException("do not call cast except in expression.");
+        }
 
         public static TSelect Cast<TDB, TSelect>(this IQuery<TDB, TSelect> query)
             where TDB : class
-            where TSelect : class 
-            => default(TSelect);
+            where TSelect : class
+        {
+            throw new NotSupportedException("do not call cast except in expression.");
+        }
 
         public static IDbExecutor<TSelect> ToExecutor<TDB, TSelect>(this IQuery<TDB, TSelect> query, IDbAdapter adaptor)
              where TDB : class
