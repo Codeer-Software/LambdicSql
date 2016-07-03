b7c0acec2bf1b4e7572ed09ba38f748331d86a54
石川達也 <stone.guitar.777@gmail.com>
2016-07-03 20:59:11 +0900

    refactoring.

---
+++ Project/LambdicSql/Clause/Update/UpdateClause.cs
@@ -0,0 +1,10 @@
+﻿using LambdicSql.QueryBase;
+
+namespace LambdicSql.Clause.Update
+{
+    public class UpdateClause : IClause
+    {
+        public IClause Clone() => this;
+        public string ToString(ISqlStringConverter decoder) => "DELETE";
+    }
+}
---
+++ Project/LambdicSql/Clause/Update/UpdateExtensions.cs
@@ -0,0 +1,14 @@
+﻿using LambdicSql.Clause.Update;
+using LambdicSql.QueryBase;
+
+namespace LambdicSql
+{
+    public static class UpdateExtensions
+    {
+        public static IQuery<TDB, TSelect, UpdateClause> Delete<TDB, TSelect>(this IQuery<TDB, TSelect> query)
+            where TDB : class
+            where TSelect : class
+            => new ClauseMakingQuery<TDB, TSelect, UpdateClause>(query, new UpdateClause());
+
+    }
+}
