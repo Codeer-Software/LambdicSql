82faaa0bdd7b04e52ce780daaaa1c598fe980f2a
石川達也 <stone.guitar.777@gmail.com>
2016-11-28 18:42:07 +0900

    add tests.

--- Project/TestCheck35/TestKeywordLimit.cs
+++ Project/TestCheck35/TestKeywordLimit.cs
@@ -28,7 +28,7 @@ namespace TestCheck35
         public void TestCleanup() => _connection.Dispose();
 
         [TestMethod, DataSource(Operation, Connection, Sheet, Method)]
-        public void Test_Limit_Offset()
+        public void Test_Limit_Offset1()
         {
             var name = _connection.GetType().FullName;
             if (name == "System.Data.SqlClient.SqlConnection") return;
@@ -55,7 +55,36 @@ OFFSET @p_1",
         }
 
         [TestMethod, DataSource(Operation, Connection, Sheet, Method)]
-        public void Test_OffsetRows_FetchNext()
+        public void Test_Limit_Offset2()
+        {
+            var name = _connection.GetType().FullName;
+            if (name == "System.Data.SqlClient.SqlConnection") return;
+            if (name == "Oracle.ManagedDataAccess.Client.OracleConnection") return;
+
+            var exp1 = Sql<DB>.Create(db =>(long)1);
+            var exp2 = Sql<DB>.Create(db => (long)3);
+            var query = Sql<DB>.Create(db =>
+                 Select(Asterisk(db.tbl_remuneration)).
+                 From(db.tbl_remuneration).
+                 OrderBy(new Asc(db.tbl_remuneration.id)).
+                 Limit(exp1).
+                 Offset(exp2)
+                 );
+
+            var datas = _connection.Query(query).ToList();
+            Assert.IsTrue(0 < datas.Count);
+            AssertEx.AreEqual(query, _connection,
+@"SELECT *
+FROM tbl_remuneration
+ORDER BY
+	tbl_remuneration.id ASC
+LIMIT @p_0
+OFFSET @p_1",
+(long)1, (long)3);
+        }
+
+        [TestMethod, DataSource(Operation, Connection, Sheet, Method)]
+        public void Test_OffsetRows_FetchNext1()
         {
             var name = _connection.GetType().FullName;
             if (name != "System.Data.SqlClient.SqlConnection" &&
@@ -82,7 +111,36 @@ FETCH NEXT @p_1 ROWS ONLY",
         }
 
         [TestMethod, DataSource(Operation, Connection, Sheet, Method)]
-        public void Test_Continue_Limit_Offset()
+        public void Test_OffsetRows_FetchNext2()
+        {
+            var name = _connection.GetType().FullName;
+            if (name != "System.Data.SqlClient.SqlConnection" &&
+                name != "IBM.Data.DB2.DB2Connection") return;
+
+            var exp1 = Sql<DB>.Create(db => (long)1);
+            var exp2 = Sql<DB>.Create(db => (long)3);
+            var query = Sql<DB>.Create(db =>
+                 Select(Asterisk(db.tbl_remuneration)).
+                 From(db.tbl_remuneration).
+                 OrderBy(new Asc(db.tbl_remuneration.id)).
+                 OffsetRows(exp1).
+                 FetchNextRowsOnly(exp2)
+                 );
+
+            var datas = _connection.Query(query).ToList();
+            Assert.IsTrue(0 < datas.Count);
+            AssertEx.AreEqual(query, _connection,
+@"SELECT *
+FROM tbl_remuneration
+ORDER BY
+	tbl_remuneration.id ASC
+OFFSET @p_0 ROWS
+FETCH NEXT @p_1 ROWS ONLY",
+(long)1, (long)3);
+        }
+
+        [TestMethod, DataSource(Operation, Connection, Sheet, Method)]
+        public void Test_Continue_Limit_Offset1()
         {
             var name = _connection.GetType().FullName;
             if (name == "System.Data.SqlClient.SqlConnection") return;
@@ -109,7 +167,36 @@ OFFSET @p_1",
         }
 
         [TestMethod, DataSource(Operation, Connection, Sheet, Method)]
-        public void Test_Continue_OffsetRows_FetchNext()
+        public void Test_Continue_Limit_Offset2()
+        {
+            var name = _connection.GetType().FullName;
+            if (name == "System.Data.SqlClient.SqlConnection") return;
+            if (name == "Oracle.ManagedDataAccess.Client.OracleConnection") return;
+
+            var exp1 = Sql<DB>.Create(db => (long)1);
+            var exp2 = Sql<DB>.Create(db => (long)3);
+            var query = Sql<DB>.Create(db =>
+                 Select(Asterisk(db.tbl_remuneration)).
+                 From(db.tbl_remuneration).
+                 OrderBy(new Asc(db.tbl_remuneration.id)));
+            var limit = Sql<DB>.Create(db => Limit(exp1));
+            var offset = Sql<DB>.Create(db => Offset(exp2));
+            query = query.Concat(limit).Concat(offset);
+
+            var datas = _connection.Query(query).ToList();
+            Assert.IsTrue(0 < datas.Count);
+            AssertEx.AreEqual(query, _connection,
+@"SELECT *
+FROM tbl_remuneration
+ORDER BY
+	tbl_remuneration.id ASC
+LIMIT @p_0
+OFFSET @p_1",
+(long)1, (long)3);
+        }
+
+        [TestMethod, DataSource(Operation, Connection, Sheet, Method)]
+        public void Test_Continue_OffsetRows_FetchNext1()
         {
             var name = _connection.GetType().FullName;
             if (name != "System.Data.SqlClient.SqlConnection" &&
@@ -136,7 +223,36 @@ FETCH NEXT @p_1 ROWS ONLY",
         }
 
         [TestMethod, DataSource(Operation, Connection, Sheet, Method)]
-        public void Test_RowNum()
+        public void Test_Continue_OffsetRows_FetchNext2()
+        {
+            var name = _connection.GetType().FullName;
+            if (name != "System.Data.SqlClient.SqlConnection" &&
+                name != "IBM.Data.DB2.DB2Connection") return;
+
+            var exp1 = Sql<DB>.Create(db => (long)1);
+            var exp2 = Sql<DB>.Create(db => (long)3);
+            var query = Sql<DB>.Create(db =>
+                 Select(Asterisk(db.tbl_remuneration)).
+                 From(db.tbl_remuneration).
+                 OrderBy(new Asc(db.tbl_remuneration.id)));
+            var offsetRows = Sql<DB>.Create(db => OffsetRows(exp1));
+            var fetchNextRowOnly = Sql<DB>.Create(db => FetchNextRowsOnly(exp2));
+            query = query.Concat(offsetRows).Concat(fetchNextRowOnly);
+
+            var datas = _connection.Query(query).ToList();
+            Assert.IsTrue(0 < datas.Count);
+            AssertEx.AreEqual(query, _connection,
+@"SELECT *
+FROM tbl_remuneration
+ORDER BY
+	tbl_remuneration.id ASC
+OFFSET @p_0 ROWS
+FETCH NEXT @p_1 ROWS ONLY",
+(long)1, (long)3);
+        }
+
+        [TestMethod, DataSource(Operation, Connection, Sheet, Method)]
+        public void Test_RowNum1()
         {
             var name = _connection.GetType().FullName;
             if (name != "Oracle.ManagedDataAccess.Client.OracleConnection") return;
@@ -150,9 +266,40 @@ FETCH NEXT @p_1 ROWS ONLY",
 
             var datas = _connection.Query(query).ToList();
             Assert.AreEqual(3, datas.Count);
-            query.Gen(_connection);
+            AssertEx.AreEqual(query, _connection,
+ @"SELECT *
+FROM tbl_remuneration
+WHERE ROWNUM BETWEEN :p_0 AND :p_1
+ORDER BY
+	tbl_remuneration.id ASC",
+(long)1, (long)3);
         }
 
-        //TODO Expressionはそれぞれいる?
+        [TestMethod, DataSource(Operation, Connection, Sheet, Method)]
+        public void Test_RowNum2()
+        {
+            var name = _connection.GetType().FullName;
+            if (name != "Oracle.ManagedDataAccess.Client.OracleConnection") return;
+
+            var exp1 = Sql<DB>.Create(db => RowNum);
+            var exp2 = Sql<DB>.Create(db => (long)1);
+            var exp3 = Sql<DB>.Create(db => (long)3);
+            var query = Sql<DB>.Create(db =>
+                 Select(Asterisk(db.tbl_remuneration)).
+                 From(db.tbl_remuneration).
+                 Where(Between(exp1, exp2, exp3)).
+                 OrderBy(new Asc(db.tbl_remuneration.id))
+                 );
+
+            var datas = _connection.Query(query).ToList();
+            Assert.AreEqual(3, datas.Count);
+            AssertEx.AreEqual(query, _connection,
+ @"SELECT *
+FROM tbl_remuneration
+WHERE ROWNUM BETWEEN :p_0 AND :p_1
+ORDER BY
+	tbl_remuneration.id ASC",
+(long)1, (long)3);
+        }
     }
 }
