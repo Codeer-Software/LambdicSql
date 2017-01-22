f1d1be8231d52a2f11ced5a062f4fecac6e89eda
石川達也 <stone.guitar.777@gmail.com>
2017-01-22 15:03:03 +0900

    refactoring.

--- Project/LambdicSql/MultiplatformCompatibe/BuildedSqlExtensions.cs
+++ Project/LambdicSql/MultiplatformCompatibe/BuildedSqlExtensions.cs
@@ -3,25 +3,21 @@
 namespace LambdicSql
 {
     /// <summary>
-    /// 
+    /// Extensions for BuildedSql.
     /// </summary>
     public static class BuildedSqlExtensions
     {
         /// <summary>
-        /// 
+        /// Get parameters.
         /// </summary>
-        /// <param name="sql"></param>
-        /// <returns></returns>
+        /// <param name="sql">BuildedSql.</param>
+        /// <returns>Parameters.</returns>
         public static Dictionary<string, DbParam> GetParams(this BuildedSql sql)
         {
             return sql.GetParams(e =>
             {
                 var dbParam = e as DbParam;
-                if (dbParam == null)
-                {
-                    dbParam = new DbParam { Value = e.Value };
-                }
-                return dbParam;
+                return dbParam == null ? new DbParam { Value = e.Value } : dbParam;
             });
         }
     }
