f5f6f07559aee8ae50455747d6201c3a206438a3
Tatsuya Ishikawa <stone.guitar.777@gmail.com>
2017-04-16 18:35:31 +0900

    version up.

--- Project/LambdicSql.PCL/Properties/AssemblyInfo.cs
+++ Project/LambdicSql.PCL/Properties/AssemblyInfo.cs
@@ -13,5 +13,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("ja")]
 
-[assembly: AssemblyVersion("0.39.0.0")]
-[assembly: AssemblyFileVersion("0.39.0.0")]
+[assembly: AssemblyVersion("0.40.0.0")]
+[assembly: AssemblyFileVersion("0.40.0.0")]
