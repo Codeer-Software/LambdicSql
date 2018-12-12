2dda7a7e22381f8109e17fb2857d528a6700e86c
Tatsuya Ishikawa <ishikawa-tatsuya@codeer.co.jp>
2018-12-12 14:38:28 +0900

    Bug fix nuspec.

--- Project/LambdicSql.PCL/Properties/AssemblyInfo.cs
+++ Project/LambdicSql.PCL/Properties/AssemblyInfo.cs
@@ -13,5 +13,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("ja")]
 
-[assembly: AssemblyVersion("0.63.0.0")]
-[assembly: AssemblyFileVersion("0.63.0.0")]
+[assembly: AssemblyVersion("0.64.0.0")]
+[assembly: AssemblyFileVersion("0.64.0.0")]
